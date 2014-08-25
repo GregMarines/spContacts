c9b764054ad743ed42abef8c850e8d552717984f
Greg Marines <gregrm@gmail.com>
2014-08-24 21:27:37 -0400

    bump

--- www/js/index.js
+++ www/js/index.js
@@ -33,18 +33,19 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+       // app.receivedEvent('deviceready');
+		angular.bootstrap(document, ["myApp"]);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
+       // var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
        // var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
+        //listeningElement.setAttribute('style', 'display:none;');
        // receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-		angular.bootstrap(document, ["myApp"]);
+        //console.log('Received Event: ' + id);
+		//angular.bootstrap(document, ["myApp"]);
     }
 };

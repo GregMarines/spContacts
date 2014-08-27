4260cb98d456b781912202925746571bcaa052b0
Greg Marines <gregrm@gmail.com>
2014-08-27 02:27:04 -0400

    added android push notifications

--- www/js/index.js
+++ www/js/index.js
@@ -34,12 +34,43 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		var pushNotification = window.plugins.pushNotification;
+		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"746935138522","ecb":"app.onNotificationGCM"});
+		
 		
     },
     // Update DOM on a Received Event
 	receivedEvent: function(id) {
 		console.log('Received Event: ' + id);
 		angular.bootstrap(document, ["myApp"]);
+	}, // result contains any message sent from the plugin call
+	successHandler: function(result) {
+		alert('Callback Success! Result = '+result)
+	},
+	errorHandler:function(error) {
+		alert(error);
+	},
+	onNotificationGCM: function(e) {
+		switch( e.event )
+		{
+			case 'registered':
+				if ( e.regid.length > 0 )
+				{
+					console.log("Regid " + e.regid);
+					alert('registration id = '+e.regid);
+				}
+				break;
+			case 'message':
+				// this is the actual push notification. its format depends on the data model from the push server
+				alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+				break;
+			case 'error':
+				alert('GCM error = '+e.msg);
+				break;
+			default:
+				alert('An unknown GCM event has occurred');
+				break;
+		}
 	}
     //receivedEvent: function(id) {
         //var parentElement = document.getElementById(id);

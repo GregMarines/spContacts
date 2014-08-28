9d6a2351888dd49b0bbaea6ad3168bf28f5a3634
Greg Marines <gregrm@gmail.com>
2014-08-27 20:56:17 -0400

    bump

--- www/js/index.js
+++ www/js/index.js
@@ -34,53 +34,47 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		//var pushNotification = window.plugins.pushNotification;
-		//pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"746935138522","ecb":"app.onNotificationGCM"});
-		
+		var pushNotification = window.plugins.pushNotification;
+		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"746935138522","ecb":"app.onNotificationGCM"});		
 		
     },
-    // Update DOM on a Received Event
-	receivedEvent: function(id) {
-		console.log('Received Event: ' + id);
-		angular.bootstrap(document, ["myApp"]);
-	}, // result contains any message sent from the plugin call
-	successHandler: function(result) {
-		alert('Callback Success! Result = '+result)
-	},
-	errorHandler:function(error) {
-		alert(error);
-	},
-	onNotificationGCM: function(e) {
-		switch( e.event )
-		{
-			case 'registered':
-				if ( e.regid.length > 0 )
-				{
-					console.log("Regid " + e.regid);
-					alert('registration id = '+e.regid);
-				}
-				break;
-			case 'message':
-				// this is the actual push notification. its format depends on the data model from the push server
-				alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-				break;
-			case 'error':
-				alert('GCM error = '+e.msg);
-				break;
-			default:
-				alert('An unknown GCM event has occurred');
-			break;
-	}
-	}
-    //receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-       //var listeningElement = parentElement.querySelector('.listening');
-       // var receivedElement = parentElement.querySelector('.received');
-
-        //listeningElement.setAttribute('style', 'display:none;');
-       // receivedElement.setAttribute('style', 'display:block;');
-
-        //console.log('Received Event: ' + id);
-		//angular.bootstrap(document, ["myApp"]);
-    //}
+   // Update DOM on a Received Event
+receivedEvent: function(id) {
+angular.bootstrap(document, ["myApp"]);
+var parentElement = document.getElementById(id);
+//var listeningElement = parentElement.querySelector('.listening');
+//var receivedElement = parentElement.querySelector('.received');
+//listeningElement.setAttribute('style', 'display:none;');
+//receivedElement.setAttribute('style', 'display:block;');
+console.log('Received Event: ' + id);
+},
+// result contains any message sent from the plugin call
+successHandler: function(result) {
+alert('Callback Success! Result = '+result)
+},
+errorHandler:function(error) {
+alert(error);
+},
+onNotificationGCM: function(e) {
+switch( e.event )
+{
+case 'registered':
+if ( e.regid.length > 0 )
+{
+console.log("Regid " + e.regid);
+alert('registration id = '+e.regid);
+}
+break;
+case 'message':
+// this is the actual push notification. its format depends on the data model from the push server
+alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+break;
+case 'error':
+alert('GCM error = '+e.msg);
+break;
+default:
+alert('An unknown GCM event has occurred');
+break;
+}
+}
 };

81eae59b8c8af0d1ab8d06364242de8357937dc6
Greg Marines <gregrm@gmail.com>
2014-09-02 00:58:17 -0400

    changing pushwoosh settings

--- www/js/index.js
+++ www/js/index.js
@@ -17,19 +17,37 @@
  * under the License.
  */
  
-function initPushwoosh() {
-	var pushNotification = window.plugins.pushNotification;
-	if(device.platform == "Android")
-	{
-		registerPushwooshAndroid();
-	}
-
-	if(device.platform == "iPhone" || device.platform == "iOS")
-	{
-		registerPushwooshIOS();
-	}
-}
+function initPushwoosh()
+{
+    //get pushwoosh plugin
+    var pushNotification = window.plugins.pushNotification;
+    //notify plugin that device is ready, this is VERY important as it will dispatch on start push notification
+    pushNotification.onDeviceReady();
  
+    //register for push notifications
+    pushNotification.registerDevice({ projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID" },
+        function(status) {
+            //this is push token
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+ 
+    //this function gets called when push notifications has been received
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+            var userData = event.notification.userdata;
+                                 
+            if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+}
  
  
 var app = {

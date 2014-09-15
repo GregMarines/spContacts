03919153cd789f0eddb83b25c47323e27a725301
Greg Marines <gregrm@gmail.com>
2014-09-14 20:41:02 -0400

    bump

--- www/js/app.js
+++ www/js/app.js
@@ -24,3 +24,21 @@ function handleDeviceBackButton(){
      alert('ill never see this');
 	 angular.element('[ng-controller=myApp]').scope().goHome();
 }
+
+function AppCtrl($scope, $location) {
+    $scope.goHome = function () {
+
+        navigator.app.exitApp();   // This will exit the application       
+
+	   /*
+	   if ((window.location) && (window.location.hash) && (window.location.hash.indexOf('home') > 0)) {
+            navigator.app.exitApp();   // This will exit the application
+        }
+        else {
+            $location.path('/home');  // This will redirect the application to home page
+
+            if (!$scope.$$phase)
+                $scope.$apply();
+        }*/
+    }	
+}
